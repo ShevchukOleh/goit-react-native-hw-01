a802d56734dbd5f1dc38f528698639b0a32f8fdf
ShevchukOleh <shew4ukoleg12@gmail.com>
2023-06-16 20:46:42 +0300

    first commit

--- Screens/CreatePostsScreen.jsx
+++ Screens/CreatePostsScreen.jsx
@@ -1,99 +1,171 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import Icon from 'react-native-vector-icons/Feather';
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
 
 export default function CreatePostsScreen() {
     const [photoAdded, setPhotoAdded] = useState(false);
     const [title, setTitle] = useState("");
     const [location, setLocation] = useState("");
+    const [hasPermission, setHasPermission] = useState(null);
+    const [cameraRef, setCameraRef] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
+    const [photoUri, setPhotoUri] = useState(null);
+
+
+    useEffect(() => {
+      (async () => {
+        const { status } = await Camera.requestPermissionsAsync();
+        await MediaLibrary.requestPermissionsAsync();
+  
+        setHasPermission(status === "granted");
+      })();
+    }, []);
+  
+    if (hasPermission === null) {
+      return <View />;
+    }
+    if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
 
     return (
-        <View style={styles.section}>
-            <View style={styles.container}>
-                <TouchableOpacity style={styles.iconCamera} onPress={() => setPhotoAdded(true)} activeOpacity={0.7}>
-                <Icon name="camera" color={'#BDBDBD'} size={24}/></TouchableOpacity>
-            </View>
-            <Text style={styles.text}>Завантажте фото</Text>
-            <TextInput style={styles.input} placeholder="Назва..." value={title}
-                onChangeText={(text) => setTitle(text)}>
-            </TextInput>
-            <TextInput style={styles.input} placeholder="Місцевість..." value={location}
-                onChangeText={(text) => setLocation(text)}>
-            </TextInput>
-            <TouchableOpacity style={[styles.button, 
-                {backgroundColor: ((photoAdded && title && location) ? '#FF6C00' : '#F6F6F6')}]}
-                disabled={!photoAdded || !title || !location}>
-                <Text style={[styles.buttonText, { color: (photoAdded && title && location) ? '#fff' : '#BDBDBD' }]}>Опубліковати</Text>
-            </TouchableOpacity>
-            <View style={{justifyContent: 'center', alignItems: 'center'}}>
-                <TouchableOpacity style={styles.iconContainer}><Icon name="trash-2" color={'#DADADA'} size={24}/></TouchableOpacity>
-            </View>
+      <View style={styles.section}>
+        <View style={styles.container}>
+          {photoAdded && photoUri ? (
+            <Image source={{ uri: photoUri }} style={styles.photoPreview} />
+          ) : (
+            <Camera style={styles.camera} type={type} ref={setCameraRef}>
+              <View style={styles.photoView}>
+                <TouchableOpacity
+                  style={styles.flipContainer}
+                  onPress={() => {
+                    setType(
+                      type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back
+                    );
+                  }}
+                >
+                  <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+                    {" "}
+                    Flip{" "}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </Camera>
+          )}
+          <TouchableOpacity
+            style={styles.iconCamera}
+            onPress={async () => {
+              if (cameraRef) {
+                const { uri } = await cameraRef.takePictureAsync();
+                await MediaLibrary.createAssetAsync(uri);
+                setPhotoAdded(true);
+                setPhotoUri(uri);
+              }
+            }}
+            activeOpacity={0.7}
+          >
+            <Icon name="camera" color={"#BDBDBD"} size={24} />
+          </TouchableOpacity>
         </View>
+        <Text style={styles.text}>Завантажте фото</Text>
+        <TextInput style={styles.input} placeholder="Назва..." value={title}
+            onChangeText={(text) => setTitle(text)}>
+        </TextInput>
+        <TextInput style={styles.input} placeholder="Місцевість..." value={location}
+            onChangeText={(text) => setLocation(text)}>
+        </TextInput>
+        <TouchableOpacity style={[styles.button, 
+            {backgroundColor: ((photoAdded && title && location) ? '#FF6C00' : '#F6F6F6')}]}
+            disabled={!photoAdded || !title || !location}>
+            <Text style={[styles.buttonText, { color: (photoAdded && title && location) ? '#fff' : '#BDBDBD' }]}>Опубліковати</Text>
+        </TouchableOpacity>
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <TouchableOpacity style={styles.iconContainer} onPress={() => {setPhotoAdded(false); setTitle(''); setLocation('');}}>
+            <Icon name="trash-2" color={'#DADADA'} size={24}/>
+          </TouchableOpacity>
+        </View>
+      </View>
     )
 }
 
 const styles = StyleSheet.create({
     section: {
-        flex:1,
-        paddingHorizontal: 16,
-        paddingTop: 32,
-        paddingBottom: 34,
-        backgroundColor: 'white',
+      flex:1,
+      paddingHorizontal: 16,
+      paddingTop: 32,
+      paddingBottom: 34,
+      backgroundColor: 'white',
     },
     container: {
-        flex: 1,
-        height: 240,
-        backgroundColor: '#F6F6F6',
-        justifyContent: 'center',
-        alignItems: 'center',
+      flex: 1,
+      height: 240,
+      backgroundColor: '#F6F6F6',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    camera: { 
+      flex: 1,
+      width: '100%',
+      height: '100%',
     },
     text: {
-        color:'#BDBDBD',
-        fontFamily: 'Roboto-Regular',
-        fontSize: 16,
-        lineHeight: 19,
-        marginTop: 8,
-        marginBottom: 33,
+      color:'#BDBDBD',
+      fontFamily: 'Roboto-Regular',
+      fontSize: 16,
+      lineHeight: 19,
+      marginTop: 8,
+      marginBottom: 33,
     },
     input: {
-        color:'#BDBDBD',
-        fontFamily: 'Roboto-Regular',
-        fontSize: 16,
-        lineHeight: 19,
-        paddingVertical: 15,
-        borderBottomWidth: 1,
-        borderBottomColor: '#E8E8E8',
+      color:'#BDBDBD',
+      fontFamily: 'Roboto-Regular',
+      fontSize: 16,
+      lineHeight: 19,
+      paddingVertical: 15,
+      borderBottomWidth: 1,
+      borderBottomColor: '#E8E8E8',
     },
     button: {
-        color: "#FF6C00",
-        borderRadius: 100,
-        backgroundColor: '#FF6C00',
-        paddingTop: 16,
-        paddingBottom: 16,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 32,
-        marginBottom: 120,
+      color: "#FF6C00",
+      borderRadius: 100,
+      backgroundColor: '#FF6C00',
+      paddingTop: 16,
+      paddingBottom: 16,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 32,
+      marginBottom: 120,
     },
     buttonText: {
-        color: '#fff',
-        fontFamily: 'Roboto-Regular',
-        fontSize: 16,
-        lineHeight: 19,
-        textAlign: 'center',
+      color: '#fff',
+      fontFamily: 'Roboto-Regular',
+      fontSize: 16,
+      lineHeight: 19,
+      textAlign: 'center',
     },
     iconContainer: {
-        paddingHorizontal: 23,
-        paddingVertical: 8,
-        borderRadius: 20,
-        backgroundColor: '#F6F6F6',
+      paddingHorizontal: 23,
+      paddingVertical: 8,
+      borderRadius: 20,
+      backgroundColor: '#F6F6F6',
     },
     iconCamera: {
-        width: 60,
-        height: 60,
-        backgroundColor: '#fff',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 90,
+      width: 60,
+      height: 60,
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 90,
+      position: 'absolute',
+      top: 180,
+    },
+    photoPreview: {
+      flex: 1,
+      width: "100%",
+      height: "100%",
     },
 });
---
+++ components/Navigator.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from 'react-native-vector-icons/Feather';
+import PostsScreen from "../Screens/PostsScreen";
+import CreatePostsScreen from "../Screens/CreatePostsScreen";
+import ProfileScreen from "../Screens/ProfileScreen";
+import { useNavigation } from "@react-navigation/native";
+
+const Tabs = createBottomTabNavigator();
+
+export default function Navigation() {
+  const navigation = useNavigation();
+
+  return (
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          let iconColor = color;
+          let iconBackground = focused ? '#FF6C00' : 'transparent';
+
+          if (route.name === 'PostsScreen') {
+            iconName = 'grid';
+          } else if (route.name === 'CreatePostsScreen') {
+            iconName = 'plus';
+          } else if (route.name === 'ProfileScreen') {
+            iconName = 'user';
+          }
+          return (
+            <View style={[styles.tabIconContainer, { backgroundColor: iconBackground }]}>
+              <Icon name={iconName} size={size} color={iconColor} />
+            </View> 
+          )    
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'white',
+        inactiveTintColor: 'gray',
+        labelStyle: { display: 'none' },
+        style: {
+          backgroundColor: '#fff',
+        },
+        tabStyle: {
+          height: 83,
+          paddingTop: 9,
+          paddingBottom: 34,
+        },
+      }}
+    >
+      <Tabs.Screen name="PostsScreen" component={PostsScreen} options={{ title: 'Публікації', 
+        headerRight: () => (
+          <TouchableOpacity style={{ paddingRight: 16 }}><Icon name="log-out" color={'#BDBDBD'} size={24} /></TouchableOpacity>
+        ),
+      }}/>
+      <Tabs.Screen name="CreatePostsScreen" component={CreatePostsScreen} options={{ title: 'Створити публікацію', 
+        headerLeft: () => (
+          <TouchableOpacity style={{ paddingLeft: 16 }} onPress={() => navigation.goBack()}>
+            <Icon name="arrow-left" color={'#212121CC'} size={24} />
+          </TouchableOpacity>
+        ),
+        tabBarStyle: { display: 'none' },
+      }}/>
+      <Tabs.Screen name="ProfileScreen" component={ProfileScreen} options={{ headerShown: false }}/>
+    </Tabs.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  tabIconContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 16,
+    padding: 8,
+    width: 70,
+    height: 40,
+    borderRadius: 20,
+  },
+});
